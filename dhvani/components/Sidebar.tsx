7bd1227e53efd04e76c9cf164a1935f09703c4ec
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-11-19 06:37:18 +0530

    Update Sidebar.tsx

--- dhvani/components/Sidebar.tsx
+++ dhvani/components/Sidebar.tsx
@@ -1,9 +1,64 @@
-import Image from 'next/image'
+import { HomeIcon } from "@heroicons/react/24/outline"
+import { BuildingLibraryIcon } from "@heroicons/react/24/outline"
+import { PlusCircleIcon } from "@heroicons/react/24/outline"
+import { MagnifyingGlassCircleIcon } from "@heroicons/react/24/outline"
+import { HeartIcon } from "@heroicons/react/24/outline"
+import { RssIcon } from "@heroicons/react/24/outline"
 
 export default function Sidebar() {
   return (
-    <div>
-        <h1>I am a Sidebar.</h1>
+    <div className="text-gray-500 p-5 text-sm border-r border-gray-900">
+        <div className="space-y-4">
+
+          {/* First Section */}
+          <button className="flex items-center space-x-2 hover:text-white">
+            <HomeIcon className="w-5 h-5" />
+            <p>Home</p>
+          </button>
+          <button className="flex items-center space-x-2 hover:text-white">
+            <MagnifyingGlassCircleIcon className="w-5 h-5" />
+            <p>Search</p>
+          </button>
+          <button className="flex items-center space-x-2 hover:text-white">
+            <BuildingLibraryIcon className="w-5 h-5" />
+            <p>Your Library</p>
+          </button>
+
+          <hr className="border-t-[0.1px] border-gray-900" />
+
+          {/* Second Section */}
+          <button className="flex items-center space-x-2 hover:text-white">
+            <PlusCircleIcon className="w-5 h-5" />
+            <p>Create Playlist</p>
+          </button>
+          <button className="flex items-center space-x-2 hover:text-white">
+            <HeartIcon className="w-5 h-5" />
+            <p>Liked Songs</p>
+          </button>
+          <button className="flex items-center space-x-2 hover:text-white">
+            <RssIcon className="w-5 h-5" />
+            <p>Your Episodes</p>
+          </button>
+          
+          <hr className="border-t-[0.1px] border-gray-900" />
+
+          {/* Playlists */}
+          <p className="cursor-pointer hover:text-white">
+            Playlist name .....
+          </p>
+          <p className="cursor-pointer hover:text-white">
+            Playlist name .....
+          </p>
+          <p className="cursor-pointer hover:text-white">
+            Playlist name .....
+          </p>
+          <p className="cursor-pointer hover:text-white">
+            Playlist name .....
+          </p>
+          <p className="cursor-pointer hover:text-white">
+            Playlist name .....
+          </p>
+        </div>
     </div>
   )
 }
