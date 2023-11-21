6861a3636b6bf91fc66618760d7c9ed97e1594ec
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-11-21 10:33:04 +0530

    Update Center.tsx

--- dhvani/components/Center.tsx
+++ dhvani/components/Center.tsx
@@ -1,9 +1,9 @@
-import Image from 'next/image'
-
 export default function Center() {
   return (
-    <div>
+    <div className="w-[75%] h-full">
+      <div className="text-white text-[2rem] h-full w-full bg-[#181818] border-2 border-[#181818] p-2 my-2 rounded-lg">
         <h1>I am a Center.</h1>
+      </div>
     </div>
   )
 }
