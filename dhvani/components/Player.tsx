8e8422fa8f02bc8931117cbf30468dc980aa7c3b
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-11-21 10:33:07 +0530

    Update Player.tsx

--- dhvani/components/Player.tsx
+++ dhvani/components/Player.tsx
@@ -2,8 +2,8 @@ import Image from 'next/image'
 
 export default function Player() {
   return (
-    <div>
-        <h1>I am a Player.</h1>
+    <div className='relative bottom-100 w-full h-[110px] flex flex-row bg-[#000000]'>
+        <h1 className='text-white'>I am a Player.</h1>
     </div>
   )
 }
