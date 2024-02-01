660d5f900dc5f031cd1b010ad1de169b36a6cdda
the-bipu <127449591+the-bipu@users.noreply.github.com>
2024-02-01 09:54:34 +0530

    udpate layout

--- dhvani/app/layout.tsx
+++ dhvani/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { UserProvider } from '@/common/contexts/userContext';
 
 import './globals.css'
 
@@ -18,7 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        {children}
+        <UserProvider>
+          {children}
+        </UserProvider>
       </body>
     </html>
   )
--- dhvani/app/page.tsx
+++ dhvani/app/page.tsx
@@ -1,17 +1,22 @@
-import Center from '@/components/Center'
-import Player from '@/components/Player'
-import Sidebar from '@/components/Sidebar'
+'use client';
+import Center from '@/common/components/Center'
+import Player from '@/common/components/Player'
+import Sidebar from '@/common/components/Sidebar'
+
+import '../app/globals.css'
+import { useContext } from 'react';
+import { UserContext } from '@/common/contexts/userContext';
 
 export default function Home() {
+  const { showSideBar, setShowSideBar } = useContext(UserContext);
+
   return (
-    <main className='bg-black h-screen overflow-hidden flex flex-col'>
-      <div className='flex flex-row h-[850%]'>
-          <Sidebar />
-
-          <Center />
+    <main className=' h-screen overflow-hidden flex flex-row'>
+      <div className={`h-full ${showSideBar ? 'hidden' : 'w-1/4'}`}>
+        <Sidebar />
       </div>
-
-      <div className='flex flex-row h-[15%]'>
+      <div className={`bg-gray-300 h-full ${showSideBar ? 'w-full' : 'w-3/4'}`}>
+        <Center />
         <Player />
       </div>
     </main>
